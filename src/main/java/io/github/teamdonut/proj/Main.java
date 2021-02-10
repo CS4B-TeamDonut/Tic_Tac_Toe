3c84ffb35022d115fc8a30d01c1659adfb406fdd
joeyCampbelll <joey.m.campbell@gmail.com>
2021-02-10 13:01:41 -0800

    board class done

--- src/main/java/io/github/teamdonut/proj/Main.java
+++ src/main/java/io/github/teamdonut/proj/Main.java
@@ -11,8 +11,13 @@ public class Main extends Application {
     }
 
     public static void main(String[] args) {
+
+        Board test1 = new Board();
+        Board test2 = new Board();
+        test1.updatePosition(1, 0, 'X');
+        test1.updatePosition(1,0,'O');
+        System.out.println(test1);
         launch(args);
     }
-
 }
 

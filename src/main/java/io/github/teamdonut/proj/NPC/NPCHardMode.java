b795c7733314eb7a7c545c97b5d46a8ac36c9c1f
utsavparajuli <73618451+utsavparajuli@users.noreply.github.com>
2021-02-10 21:45:12 -0800

    NPC added

---
+++ src/main/java/io/github/teamdonut/proj/NPC/NPCHardMode.java
@@ -0,0 +1,13 @@
+package io.github.teamdonut.proj.NPC;
+
+public class NPCHardMode implements NPC{
+
+    //constructor
+    public NPCHardMode() {
+    }
+
+    @Override
+    public void makeMove(char[][] board, char c) {
+
+    }
+}

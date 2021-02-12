c32813116a01017417141880cd23d783a596b9de
joeyCampbelll <joey.m.campbell@gmail.com>
2021-02-12 15:02:43 -0800

    updated board class

--- src/main/java/io/github/teamdonut/proj/Board.java
+++ src/main/java/io/github/teamdonut/proj/Board.java
@@ -30,6 +30,10 @@ public class Board {
         return board[x][y];
     }
 
+    public void clearBoard() {
+        for (char[] row: board) Arrays.fill(row, EMPTY);
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
--- src/main/java/io/github/teamdonut/proj/Player.java
+++ src/main/java/io/github/teamdonut/proj/Player.java
@@ -1,9 +1,48 @@
 package io.github.teamdonut.proj;
 
-public class Player {
+import java.util.Objects;
 
+public class Player {
     private String playerName;
     private char playerToken;
 
+    public Player() { }
 
+    public Player(String playerName) {
+        this.playerName = playerName;
+    }
+
+    public Player(String playerName, char playerToken) {
+        this.playerName = playerName;
+        this.playerToken = playerToken;
+    }
+
+    public String getPlayerName() { return playerName; }
+
+    public char getPlayerToken() { return playerToken; }
+
+    public void setPlayerName(String playerName) { this.playerName = playerName; }
+
+    public void setPlayerToken(char playerToken) { this.playerToken = playerToken; }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Player player = (Player) o;
+        return playerToken == player.playerToken && Objects.equals(playerName, player.playerName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(playerName, playerToken);
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" +
+                "playerName='" + playerName + '\'' +
+                ", playerToken=" + playerToken +
+                '}';
+    }
 }

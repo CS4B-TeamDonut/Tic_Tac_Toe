218ebf37367ceac4487e2af7ee2e6a67ddc30abb
joeyCampbelll <joey.m.campbell@gmail.com>
2021-02-12 19:17:39 -0800

    about to push

--- src/main/java/io/github/teamdonut/proj/Main.java
+++ src/main/java/io/github/teamdonut/proj/Main.java
@@ -11,16 +11,6 @@ public class Main extends Application {
     }
 
     public static void main(String[] args) {
-        Board test = new Board();
-
-        test.updatePosition(0, 1, 'X');
-
-        System.out.println(test);
-
-        test.clearBoard();
-
-        System.out.println(test);
-
         launch(args);
     }
 }

05588ecfea8209d5a2eaf6d28f5add72c4ec82af
Brandon Nguyen <nguyen.brandon771@gmail.com>
2021-02-14 22:24:33 -0800

    game class unfinished

--- src/main/java/io/github/teamdonut/proj/Game.java
+++ src/main/java/io/github/teamdonut/proj/Game.java
@@ -1,11 +1,11 @@
 package io.github.teamdonut.proj;
 
-//public class Game {
-//
-//    private final char currentPlayer = 'X';
-//    private final Board[][] board = new Board[3][3];
-//
-//
+public class Game {
+
+    private Board board;
+    private Player player1;
+    private Player player2;
+
 //    public boolean isBoardFull() {
 //        for (int i = 0; i < 3; i++) {
 //            for (int j = 0; j < 3; j++) {
@@ -16,22 +16,12 @@ package io.github.teamdonut.proj;
 //        }
 //        return true;
 //    }
-//
-//    public char currentPlayer(char currentPlayer) {
-//        if (currentPlayer == 'X') {
-//            return 'O';
-//        }
-//        else {
-//            return 'X';
-//        }
-//
-//    }
-//
+
 //    public boolean hasWon(char player) {
 //        for (int i = 0; i < 3; i++) {
 //            if(board[i][[0].getPlayer == player && board[i][1].getPlayer() == player && board[i][2].getPlayer() == player) {
 //                return true;
-//                System.out.println(player + " has won");
+//               System.out.println(player + " has won");
 //            }
 //        }
 //
@@ -42,40 +32,15 @@ package io.github.teamdonut.proj;
 //            }
 //        }
 //
-//        if(board[0][0].getPlayer() == player && board[1][1].getPlayer() == player && board[2][2].getPlayer() == player) {
+//        if (board[0][0].getPlayer() == player && board[1][1].getPlayer() == player && board[2][2].getPlayer() == player) {
 //            return true;
 //            System.out.println(player + " has won");
 //        }
 //
-//        if(board[0][2].getPlayer() == player && board[1][1].getPlayer() == player && board[2][0].getPlayer() == player) {
+//        if (board[0][2].getPlayer() == player && board[1][1].getPlayer() == player && board[2][0].getPlayer() == player) {
 //            return true;
 //            System.out.println(player + "has won");
 //        }
-//
-//
 //        return false;
 //    }
-//
-//    public static class Board {
-//
-//        private char player = ' ';
-//
-//        public char getPlayer() {
-//            return player;
-//        }
-//
-//        public void setPlayer(char c) {
-//            player = c;
-//
-//            if (player == 'X') {
-//                System.out.println("X");
-//
-//            } else if (player == 'O') {
-//                System.out.println("O");
-//            }
-//        }
-//
-//
-//    }
-//
-//}
+}

6b1c2ce5a427b792d4a2423f5455937d9871ab6b
Grant Goldsworth <goldsworthgrant@gmail.com>
2021-02-14 16:01:53 -0800

    added sp/mp buttons to main menu, need new graphics + doc

--- src/main/java/io/github/teamdonut/proj/MainController.java
+++ src/main/java/io/github/teamdonut/proj/MainController.java
@@ -2,25 +2,31 @@ package io.github.teamdonut.proj;
 
 import io.github.teamdonut.proj.listener.EventManager;
 import io.github.teamdonut.proj.listener.ISubject;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 /**
  * Main Screen Controller class
- * @author Kord Boniadi
+ * @author Grant Goldsworth
  */
 public class MainController implements Initializable, ISubject {
     @FXML
-    private Label label;
+    public VBox mainMenuPane;
 
     @FXML
-    private BorderPane borderPane;
+    private ImageView singlePlayerButton;
+
+    @FXML
+    private ImageView multiPlayerButton;
+
 
     /*
      *  // By pass the need for this:
@@ -58,26 +64,53 @@ public class MainController implements Initializable, ISubject {
 //        String javaVersion = System.getProperty("java.version");
 //        String javafxVersion = System.getProperty("javafx.version");
 //        label.setText("Hello, JavaFX " + javafxVersion + "\nRunning on Java " + javaVersion + ".");
+
+        // create single player button graphic
+        // obtain input
+//        ImageView singleplayerView = new ImageView(new Image(getClass().getResourceAsStream("images/theme_1/singleplayer_button.png")));
+//        singleplayerView.setPreserveRatio(true);
+//        singleplayerView.setFitHeight(120);
+//        singleplayerView.setFitWidth(40);
+//        singlePlayerButton.setGraphic(singleplayerView);
+//        singlePlayerButton.setPrefSize(120, 40);
+
     }
 
 
-    /**
-     * Event handler that's triggered on "start game" button click
-     * @param actionEvent javafx specific
-     * @see EventManager#notify(ISubject, Object)
-     * @author Kord Boniadi
-     */
-    public void onGameButtonClick(ActionEvent actionEvent) {
+    public void onSinglePlayerButtonClick(/*MouseEvent mouseEvent*/) {
         Board board = new Board();
         EventManager.notify(this, board);
 
-//        Parent root = FXMLLoader.load(getClass().getResource("boardPage.fxml"));
-//        Stage window = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
-////        Stage window = (Stage) gameButton.getScene().getWindow();
-//        Scene scene = new Scene(root);
-//        scene.getStylesheets().add((getClass().getResource("styles.css")).toExternalForm());
-//        window.setScene(scene);
-//        window.setResizable(false);
-//        window.show();
+        //        Parent root = FXMLLoader.load(getClass().getResource("boardPage.fxml"));
+        //        Stage window = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        //        Stage window = (Stage) gameButton.getScene().getWindow();
+        //        Scene scene = new Scene(root);
+        //        scene.getStylesheets().add((getClass().getResource("styles.css")).toExternalForm());
+        //        window.setScene(scene);
+        //        window.setResizable(false);
+        //        window.show();
     }
+
+    public void onSinglePlayerButtonHover(/*MouseEvent mouseEvent*/) {
+        singlePlayerButton.setImage(new Image(getClass().getResourceAsStream("images/theme_1/singleplayer_button_hover.png")));
+    }
+
+    public void onSinglePlayerButtonExit(/*MouseEvent mouseEvent*/) {
+        singlePlayerButton.setImage(new Image(getClass().getResourceAsStream("images/theme_1/singleplayer_button.png")));
+    }
+
+
+
+    public void onMultiPlayerButtonClick(/*MouseEvent mouseEvent*/) {
+
+    }
+
+    public void onMultiPlayerButtonHover(/*MouseEvent mouseEvent*/) {
+        multiPlayerButton.setImage(new Image(getClass().getResourceAsStream("images/theme_1/multiplayer_button_hover.png")));
+    }
+
+    public void onMultiPlayerButtonExit(/*MouseEvent mouseEvent*/) {
+        multiPlayerButton.setImage(new Image(getClass().getResourceAsStream("images/theme_1/multiplayer_button.png")));
+    }
+
 }

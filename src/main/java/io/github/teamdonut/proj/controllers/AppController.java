498e748656276021f67599c74af41a250c592152
utsavparajuli <73618451+utsavparajuli@users.noreply.github.com>
2021-02-20 14:57:45 -0800

    intermediate fxml file changed

--- src/main/java/io/github/teamdonut/proj/controllers/AppController.java
+++ src/main/java/io/github/teamdonut/proj/controllers/AppController.java
@@ -12,6 +12,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
@@ -93,6 +94,12 @@ public class AppController implements IObserver {
 
             TextField nameEntry = new TextField();
             nameEntry.setId("nameEntry");
+
+            nameEntry.setOnKeyPressed(event -> {
+                if(event.getCode() == KeyCode.ENTER) {
+                    update(new GameController());
+                }
+            });
 
             Button entry = new Button("Enter");
             entry.setId("entry");

d79270b60901bed890369884219ed3c15999e064
Grant Goldsworth <goldsworthgrant@gmail.com>
2021-02-24 17:53:16 -0800

    added makeMove() impl to hard mode, untested

--- src/main/java/io/github/teamdonut/proj/NPC/NPCHardMode.java
+++ src/main/java/io/github/teamdonut/proj/NPC/NPCHardMode.java
@@ -1,6 +1,7 @@
 package io.github.teamdonut.proj.NPC;
 
 import io.github.teamdonut.proj.common.Board;
+import io.github.teamdonut.proj.listener.EventManager;
 import io.github.teamdonut.proj.utils.DataValidation;
 
 public class NPCHardMode implements NPC {
@@ -22,14 +23,38 @@ public class NPCHardMode implements NPC {
     @Override
     public void makeMove(Board board, char c) {
         DataValidation.ensureObjectNotNull("Board", board);
-        int x;  // x coordinate of move to make
-        int y;  // y coordinate of move to make
-        char[][] localBoard = board.getUnderlyingBoard(); // get the actual board data
+        int moveRow = 0;      // moveRow coordinate of move to make
+        int moveCol = 0;      // moveCol coordinate of move to make
+        int bestValue = -100; // starting best value of the AI move
 
-        // call recursive minimax algorithm function
+        // for each cell
+        for (int row = 0; row < 3; row ++) {
+            for (int col = 0; col < 3; col ++) {
+                // if the cell is empty
+                if(board.getToken(row, col) == ' ') {
+                    // simulate the player's move here (or maximizer's move)
+                    board.updateToken(row, col, NPCHardMode.MAXIMIZER);
 
-        // pass move to new BoardInfo thing
-        // notify manager
+                    // run minimax on this spot and record result
+                    int miniMaxResult = NPCHardMode.miniMax(board, 0, false);
+
+                    // undo the move for this iteration
+                    board.updateToken(row, col, ' ');
+
+                    // if the current move's value from minimax is better than the bestValue, update
+                    // the best value and record location
+                    // should end with value 10 and move [2,2]
+                    if (bestValue < miniMaxResult) {
+                        bestValue = miniMaxResult;
+                        moveRow = row;
+                        moveCol = col;
+                    }
+                }
+            }
+        } // end for each cell in board
+
+        // TODO verify that this is how its supposed to be done
+        EventManager.notify(this, new NPC.BoardMoveInfo(moveRow, moveCol, c));
     }
 
 
@@ -41,6 +66,7 @@ public class NPCHardMode implements NPC {
      * @param depth the current depth of the recursion; used to increase efficiency
      * @param isMaximizer whether or not current call is for the maximizer's turn
      * @return value chosen at current node by AI
+     * @author Grant Goldsworth
      */
     public static int miniMax(Board board, int depth, boolean isMaximizer) {
 
@@ -114,6 +140,7 @@ public class NPCHardMode implements NPC {
      * the minimizer has won, and 0 if there is a draw or no win/loss.
      * @param board the board with the current state of the game
      * @return 10 if max win, 0 if draw/none, -10 if min win
+     * @author Grant Goldsworth
      */
     public static int evaluate(Board board) {
         // check rows for X or O victory
@@ -171,6 +198,7 @@ public class NPCHardMode implements NPC {
      * is not the character ' ').
      * @param board the board to test
      * @return true if all cells have a value besides ' ', false otherwise
+     * @author Grant Goldsworth
      */
     public static boolean isFullBoard(Board board) {
         for (char[] row : board.getUnderlyingBoard()) {
